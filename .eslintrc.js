22a7586bf2617bf61d1c328ff50f8977194e7e77
Rhan Miano <rhanmiano29@gmail.com>
2021-04-19 14:33:09 +0800

    chore: delete unnecessary files

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,8 @@ module.exports = {
   ],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
 }
