c63022c47503310b2dc7537e65075461081437a6
Rhan Miano <rhanmiano29@gmail.com>
2021-06-04 00:27:59 +0800

    chore: remove eslint-module in buildModules

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,8 +31,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
     '@nuxtjs/axios',
